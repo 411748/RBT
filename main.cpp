b853bf2a0ef5a6bce838315d9c32052fc619d204
U-BSD\411748 <411748@SHS-JYBF6X3.beaverton.k12.or.us>
2024-04-30 11:07:21 -0700

    Wrote main code and added more numbers to number text file

--- main.cpp
+++ main.cpp
@@ -2,16 +2,50 @@
 #include <iostream>
 #include "tree.h"
 #include <cstring>
+#include <fstream>
 
 using namespace std;
 
 int main() {
   RedBlackTree tree;
-  tree.add(3);
-  tree.add(5);
-  tree.add(2);
-  tree.add(6);
-  tree.add(8);
-  tree.print();
+  int running = 1;
+  char choice[3];
+
+  while(running) {
+    cout << "Select an option:" << endl;
+    cout << "A: Add numbers manually" << endl;
+    cout << "AF: Add numbers from file" << endl;
+    cout << "P: Print tree" << endl;
+    cout << "Q: Quit" << endl;
+    cout << "Enter choice: ";
+    cin >> choice;
+     if(strcmp(choice, "A") == 0) {
+      int count;
+      cout << "Enter how many numbers you want to add: " << endl;
+      cin >> count;
+      cout << "Enter " << count << " numbers separated by spaces: ";
+      for(int i = 0; i < count; i++) {
+        int num;
+        cin >> num;
+        tree.add(num);
+      }
+    } else if (strcmp(choice, "AF") == 0) {
+      int numCount;
+      cout << "Enter the number of numbers you want to add from file: ";
+      cin >> numCount;
+      ifstream file("numbers.txt");
+      int num;
+      for(int i = 0; i < numCount && file >> num; i++) {
+        tree.add(num);
+      }
+      file.close();
+    } else if (strcmp(choice, "P") == 0) {
+      tree.print();
+    } else if(strcmp(choice, "Q") == 0) {
+      running = 0;
+    } else {
+      cout << "Invalid choice. Please try again." << endl;
+    }
+  }
   return 0;
 }
